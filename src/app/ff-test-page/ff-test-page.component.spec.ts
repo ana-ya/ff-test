6efc70cfb09ddcdb20b98c0d0901488f68ffdd01
anastasia <an.yakimchuk@gmail.com>
2018-07-30 09:26:56 +0300

    Add test to FfTestComponent Component

--- src/app/ff-test-page/ff-test-page.component.spec.ts
+++ src/app/ff-test-page/ff-test-page.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FfTestPageComponent } from '@app/ff-test-page/ff-test-page.component';
+import { ReactiveFormsModule } from '../../../node_modules/@angular/forms';
+import { CoreModule } from '@app/core';
+import { NgbModule } from '../../../node_modules/@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '../../../node_modules/@ngx-translate/core';
+import { RouterTestingModule } from '../../../node_modules/@angular/router/testing';
 
 describe('FfTestPageComponent', () => {
   let component: FfTestPageComponent;
@@ -8,6 +13,13 @@ describe('FfTestPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NgbModule.forRoot(),
+        RouterTestingModule,
+        TranslateModule.forRoot(),
+        ReactiveFormsModule,
+        CoreModule
+      ],
       declarations: [ FfTestPageComponent ]
     })
     .compileComponents();
